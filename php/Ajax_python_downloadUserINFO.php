f2a919d1e344a5a7488179d922e220dde31eb015
joe12334667 <joe12334667@gmail.com>
2020-11-03 17:25:27 +0800

    Update Ajax_python_downloadUserINFO.php

--- php/Ajax_python_downloadUserINFO.php
+++ php/Ajax_python_downloadUserINFO.php
@@ -1,5 +1,5 @@
 <?php 
-shell_exec("/usr/bin/python3 -c \"print('hello world')\"", $res);
+$res =  shell_exec("/usr/bin/python3 -c \"print('hello world')\"");
 //exec("python module4.py joe_try_something 3", $res);
 echo json_encode($res, JSON_UNESCAPED_UNICODE);
 
