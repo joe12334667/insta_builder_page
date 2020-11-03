4c6084b91ffe14c65045e0b6a57634e41c12f639
joe12334667 <joe12334667@gmail.com>
2020-11-03 17:38:12 +0800

    Create Ajax_python_downloadUserINFO.php

---
+++ python/Ajax_python_downloadUserINFO.php
@@ -0,0 +1,11 @@
+<?php 
+//$res = shell_exec("/usr/bin/python3 /home/bitnami/htdocs/insta_builder_page/python/ajax_downUserINFO.py");
+$res = shell_exec("pwd");
+//echo json_encode($res, JSON_UNESCAPED_UNICODE);
+var_dump($res);
+$res = shell_exec("ls");
+var_dump($res);
+//exec("python module4.py joe_try_something 3", $res);
+//echo json_encode($res, JSON_UNESCAPED_UNICODE);
+
+?>
