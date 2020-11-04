f9991d79b5d7781992db2124446736a7dc2d2bdf
joe <joe12334667@gmail.com>
2020-11-04 12:10:29 +0800

    linux python 調整

--- php/Ajax_python_downloadUserINFO.php
+++ php/Ajax_python_downloadUserINFO.php
@@ -2,8 +2,8 @@
 //$res = shell_exec("/opt/bitnami/apache/htdocs/env/bin/python3 -V 2>&1");
 //var_dump($res);
 
-$res = shell_exec("/opt/bitnami/apache/htdocs/env/bin/python3 ajax_downUserINFO.py 2>&1");
-var_dump($res);
+$res = shell_exec("/opt/bitnami/apache/htdocs/env/bin/python3  ../python/ajax_downUserINFO.py 2>&1");
+//var_dump($res);
 // $res = shell_exec("/usr/bin/python3 ajax_downUserINFO.py");
 //$res = shell_exec("pwd");
 //echo json_encode($res, JSON_UNESCAPED_UNICODE);
@@ -11,6 +11,6 @@ var_dump($res);
 //$res = shell_exec("ls");
 //var_dump($res);
 //exec("python module4.py joe_try_something 3", $res);
-//echo json_encode($res, JSON_UNESCAPED_UNICODE);
+echo json_encode($res, JSON_UNESCAPED_UNICODE);
 
 ?>
