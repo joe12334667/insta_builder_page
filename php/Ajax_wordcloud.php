66b3e0dde52d301917089444287a66df423b507c
joe12334667 <joe12334667@gmail.com>
2020-11-06 15:13:34 +0800

    去除多餘DB()與增加文字雲選項

--- php/Ajax_wordcloud.php
+++ php/Ajax_wordcloud.php
@@ -4,10 +4,16 @@ require 'DataBase.php';
 
 session_start();
 
+$cate_no = $_GET['cate_no'];
 
 //取得GET資料
 //$cate_no = 78;
 $db = DB();
-$sql = "SELECT hash_name , times FROM instabuilder.hashtags  order by times desc limit 100 offset 26 ;";
+if($cate_no == 0){
+    $sql = "SELECT hash_name , times FROM instabuilder.hashtags order by times desc limit 100 offset 26 ;";
+}else{
+    $sql = "SELECT hash_name , times FROM instabuilder.hashtags where cate_no = ".$cate_no." order by times desc limit 100 offset 1 ;";
+}
+
 $data = $db->query($sql);
 echo json_encode($data->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
