008b58ff34388e0e018480ed4855fe1d5c9fa39e
joe <joe12334667@gmail.com>
2020-12-06 18:27:27 +0800

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -790,11 +790,18 @@ if ($_SESSION["account"] == "") {
 //onLoad();
 //signOut();
             function signOut() {
-                var auth2 = gapi.auth2.getAuthInstance();
-                auth2.disconnect();
-                auth2.signOut().then(function () {
-                    console.log('User signed out.');
-                });
+                try {
+                // 需要測試的語句
+                    var auth2 = gapi.auth2.getAuthInstance();
+                    auth2.disconnect();
+                    auth2.signOut().then(function () {
+                        console.log('User signed out.');
+                    });
+                }
+                catch (e) {
+                    console.log('e);
+                }
+                
                 document.location.href = "../php/logOut.php";
 
             }
