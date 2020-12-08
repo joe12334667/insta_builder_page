d1a96237b7c08f03c58997232cbf6d2f7f0b8801
joe12334667 <joe12334667@gmail.com>
2020-12-08 12:19:44 +0800

    修改BUG

--- smart_post/smart_post.php
+++ smart_post/smart_post.php
@@ -69,8 +69,6 @@ if ($_SESSION["privilege"] != 2) {
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Settings</a>
-                        <a class="dropdown-item" href="#">Activity Log</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#" onclick="signOut();" name="logout"> Logout</a><?php echo $_SESSION["name"]; ?>-登出</a>
                     </div>
